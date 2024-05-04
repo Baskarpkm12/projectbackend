b2f26cce73e3b3b15e2aa967b05ca296d8386cfa
S.Baskar <143059377+Baskarpkm12@users.noreply.github.com>
2024-05-04 11:06:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ app.use(cors({
     origin:['http://127.0.0.1:5173', 'http://localhost:5173', ]
 }));
 app.use(cors());
-const path = require('path');
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+var storage = multer.diskStorage({
+    destination: function(req ,file ,cb){
+        cb(null, './tmp')
+    },
+    filename:function(req,file,cb){
+        cb(null , file.originalname)
+    }
+})
+var upload = multer({storage: storage})
+app.use(express.static(__dirname+'/tmp'));
+app.use('./tmp',express.static('tmp'));
+
+
+// const path = require('path');
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const productsroute = require('./routes/productsr');
 const catagoryroute = require('./routes/catagoryr');
@@ -18,6 +32,7 @@ const vendorroute = require('./routes/vendorr');
 // const saleroute = require('./routes/saler');
 // const paymentroute = require('./routes/paymentr');
 const usersroute = require('./routes/usersr');
+const multer = require('multer');
 
 app.use("/product",productsroute);
 app.use("/catagory",catagoryroute);
